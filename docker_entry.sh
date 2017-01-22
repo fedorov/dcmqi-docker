8cb4fafc634817ad3142f6cc278d397deb7bb5b3
Andrey Fedorov <fedorov@bwh.harvard.edu>
2017-01-22 14:10:39 -0500

    ENH: use full path

--- docker_entry.sh
+++ docker_entry.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 if [[ $1 = 'itkimage2paramap' ]]; then
-   itkimage2paramap "${@:2}"
+   /usr/src/dcmqi-superbuild/dcmqi-build/bin/itkimage2paramap "${@:2}"
 elif [[ $1 = 'paramap2itkimage' ]]; then
-   paramap2itkimage "${@:2}"
+   /usr/src/dcmqi-superbuild/dcmqi-build/bin/paramap2itkimage "${@:2}"
 elif [[ $1 = 'itkimage2segimage' ]]; then
-  itkimage2segimage "${@:2}"
+  /usr/src/dcmqi-superbuild/dcmqi-build/bin/itkimage2segimage "${@:2}"
 elif [[ $1 = 'segimage2itkimage' ]]; then
-  segimage2itkimage "${@:2}"
+  /usr/src/dcmqi-superbuild/dcmqi-build/bin/segimage2itkimage "${@:2}"
 elif [[ $1 = 'tid1500reader' ]]; then
-   tid1500reader "${@:2}"
+   /usr/src/dcmqi-superbuild/dcmqi-build/bin/tid1500reader "${@:2}"
 elif [[ $1 = 'tid1500writer' ]]; then
-   tid1500writer "${@:2}"
+   /usr/src/dcmqi-superbuild/dcmqi-build/bin/tid1500writer "${@:2}"
 else
   echo "Command not recognized!" \
     "Allowed commands: itkimage2paramap, " \
